0e8f1a943d6ce9640916e10e0ceeaaa099df1800
Petr Artamonov <partamonov@schubergphilis.com>
2018-10-01 13:25:52 +0200

    Migration to new client

--- cmd.go
+++ cmd.go
@@ -1,14 +1,9 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
-	"io"
-	"io/ioutil"
-	"log"
-
-	client "github.com/akamai/AkamaiOPEN-edgegrid-golang/client-v1"
 	common "github.com/apiheat/akamai-cli-common"
+	log "github.com/sirupsen/logrus"
+
 	"github.com/urfave/cli"
 )
 
@@ -20,65 +15,24 @@ func cmdReset(c *cli.Context) error {
 	return resetProperty(c)
 }
 
-func reportRespParse(in string) (data reportResponse, err error) {
-	if err = json.Unmarshal([]byte(in), &data); err != nil {
-		return
-	}
-	return
-}
-
 func reportProperty(c *cli.Context) error {
-	urlStr := fmt.Sprintf("%s/%s", URL, common.SetIntID(c, "Please provide ID for property"))
-
-	if debug {
-		println(urlStr)
-	}
-
-	data, _ := fetchData(urlStr, "GET", nil)
-
-	if debug {
-		println(data)
-	}
-
-	result, err := reportRespParse(data)
+	report, err := apiClient.A2.ReportProperty(common.SetIntID(c, "Please provide ID for property"))
 	common.ErrorCheck(err)
 
-	common.OutputJSON(result)
+	common.PrintJSON(report.Body)
 
 	return nil
 }
 
 func resetProperty(c *cli.Context) error {
-	urlStr := fmt.Sprintf("%s/%s/reset", URL, common.SetIntID(c, "Please provide ID for property"))
+	report, err := apiClient.A2.ResetProperty(common.SetIntID(c, "Please provide ID for property"))
+	common.ErrorCheck(err)
 
-	if debug {
-		println(urlStr)
-	}
-
-	_, respCode := fetchData(urlStr, "POST", nil)
-
-	if debug {
-		println(respCode)
-	}
-
-	if respCode == 204 {
-		log.Println("Reset request was completed successfully")
+	if report.Response.StatusCode == 204 {
+		log.Info("Reset request was completed successfully")
 	} else {
-		log.Printf("Something went wrong! Response code is: %v", respCode)
+		log.Info("Something went wrong! Response code is: %v", report.Response.StatusCode)
 	}
 
 	return nil
-}
-
-func fetchData(urlPath, method string, body io.Reader) (string, int) {
-	req, err := client.NewRequest(edgeConfig, method, urlPath, body)
-	common.ErrorCheck(err)
-
-	resp, err := client.Do(edgeConfig, req)
-	common.ErrorCheck(err)
-
-	defer resp.Body.Close()
-	byt, _ := ioutil.ReadAll(resp.Body)
-
-	return string(byt), resp.StatusCode
 }
--- main.go
+++ main.go
@@ -3,95 +3,22 @@ package main
 import (
 	"os"
 	"sort"
-	"time"
 
-	"github.com/akamai/AkamaiOPEN-edgegrid-golang/edgegrid"
-	"github.com/fatih/color"
-	homedir "github.com/mitchellh/go-homedir"
+	common "github.com/apiheat/akamai-cli-common"
+	edgegrid "github.com/apiheat/go-edgegrid"
+	log "github.com/sirupsen/logrus"
+
 	"github.com/urfave/cli"
 )
 
 var (
-	colorOn, raw, debug       bool
-	version, appName          string
-	configSection, configFile string
-	edgeConfig                edgegrid.Config
+	apiClient       *edgegrid.Client
+	appName, appVer string
 )
-
-// Constants
-const (
-	URL     = "/adaptive-acceleration/v1/properties"
-	padding = 3
-)
-
-type reportResponse struct {
-	CreationDate           time.Time `json:"creationDate"`
-	IsActive               bool      `json:"isActive"`
-	LastModifiedDate       time.Time `json:"lastModifiedDate"`
-	LastReset              time.Time `json:"lastReset"`
-	ZoneDeployDate         time.Time `json:"zoneDeployDate"`
-	Version                int       `json:"version"`
-	CommonPreconnectHeader []string  `json:"commonPreconnectHeader"`
-	CommonPushedResources  []string  `json:"commonPushedResources"`
-	PageSpecificRules      []struct {
-		BasePageURL                  string   `json:"basePageURL"`
-		PageSpecificPreconnectHeader []string `json:"pageSpecificPreconnectHeader"`
-		PageSpecificPushedResources  []string `json:"pageSpecificPushedResources"`
-	} `json:"pageSpecificRules"`
-}
 
 func main() {
-	_, inCLI := os.LookupEnv("AKAMAI_CLI")
-
-	appName = "akamai-a2"
-	if inCLI {
-		appName = "akamai a2"
-	}
-
-	app := cli.NewApp()
-	app.Name = appName
-	app.HelpName = appName
-	app.Usage = "A CLI to interact with Akamai Adaptive Acceleration"
-	app.Version = version
-	app.Copyright = ""
-	app.Authors = []cli.Author{
-		{
-			Name: "Petr Artamonov",
-		},
-		{
-			Name: "Rafal Pieniazek",
-		},
-	}
-
-	dir, _ := homedir.Dir()
-	dir += string(os.PathSeparator) + ".edgerc"
-
-	app.Flags = []cli.Flag{
-		cli.StringFlag{
-			Name:        "section, s",
-			Value:       "default",
-			Usage:       "`NAME` of section to use from credentials file",
-			Destination: &configSection,
-			EnvVar:      "AKAMAI_EDGERC_SECTION",
-		},
-		cli.StringFlag{
-			Name:        "config, c",
-			Value:       dir,
-			Usage:       "Location of the credentials `FILE`",
-			Destination: &configFile,
-			EnvVar:      "AKAMAI_EDGERC",
-		},
-		cli.BoolFlag{
-			Name:        "no-color",
-			Usage:       "Disable color output",
-			Destination: &colorOn,
-		},
-		cli.BoolFlag{
-			Name:        "debug",
-			Usage:       "Debug info",
-			Destination: &debug,
-		},
-	}
+	app := common.CreateNewApp(appName, "A CLI to interact with Akamai Adaptive Acceleration", appVer)
+	app.Flags = common.CreateFlags()
 
 	app.Commands = []cli.Command{
 		{
@@ -102,7 +29,7 @@ func main() {
 		},
 		{
 			Name:    "reset",
-			Aliases: []string{"rm"},
+			Aliases: []string{"re"},
 			Usage:   "Reset all existing info for property [ID]",
 			Action:  cmdReset,
 		},
@@ -112,13 +39,19 @@ func main() {
 	sort.Sort(cli.CommandsByName(app.Commands))
 
 	app.Before = func(c *cli.Context) error {
-		if c.Bool("no-color") {
-			color.NoColor = true
+		var err error
+
+		apiClient, err = common.EdgeClientInit(c.GlobalString("config"), c.GlobalString("section"), c.GlobalString("debug"))
+
+		if err != nil {
+			return cli.NewExitError(err, 1)
 		}
 
-		config(configFile, configSection)
 		return nil
 	}
 
-	app.Run(os.Args)
+	err := app.Run(os.Args)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
